34233ba1ec2929f84182008394b3c43853b56bb6
DarrenSeow <59356468+DarrenSeow@users.noreply.github.com>
2022-08-04 20:42:13 +0800

    update Render ship system

--- src/RenderingShipSystem.h
+++ src/RenderingShipSystem.h
@@ -1,7 +1,7 @@
 #pragma once
 
 
-struct RenderingPlayerShipSystem : xecs::system::instance
+struct RenderingShipSystem : xecs::system::instance
 {
     constexpr static auto typedef_v =
         xecs::system::type::child_update<RenderingSystem, RenderingSystem::update>
@@ -26,54 +26,6 @@ struct RenderingPlayerShipSystem : xecs::system::instance
     }
 
     void operator()(const Position& _position,const Scale& _scale) const noexcept
-    {
-        glColor3f(1.0f, 1.0f, 0.0f);
-
-        // Apply Transformation Matrix to quad
-        glMatrixMode(GL_MODELVIEW);
-        glPushMatrix();
-        glLoadIdentity();
-        glTranslatef(_position.m_value.m_X, _position.m_value.m_Y, 0.0f);
-        glRotatef(0.0f, 0.0f, 0.0f, -1.0f);
-        glScalef(_scale.m_value.m_X, _scale.m_value.m_Y, 0.0f);
-
-        // Render a quad
-        glBegin(GL_TRIANGLES);
-
-        glVertex2f(0.0f, -0.5f);
-        glVertex2f(0.5f, 0.5f);
-        glVertex2f(-0.5f, 0.5f);
-        glEnd();
-
-        glPopMatrix();
-    }
-};
-
-struct RenderingPlayerShipSystem : xecs::system::instance
-{
-    constexpr static auto typedef_v =
-        xecs::system::type::child_update<RenderingSystem, RenderingSystem::update>
-    {
-        .m_pName = "RenderingShipSystem"
-    };
-
-    using query = std::tuple
-        <
-        xecs::query::one_of<entity>,
-        xecs::query::must<PlayerTag>
-        >;
-
-    void OnPreUpdate(void) noexcept
-    {
-        // glBegin(GL_TRIANGLES);
-    }
-
-    void OnPostUpdate(void) noexcept
-    {
-        // glEnd();
-    }
-
-    void operator()(const Position& _position, const Scale& _scale) const noexcept
     {
         glColor3f(1.0f, 1.0f, 0.0f);
 
